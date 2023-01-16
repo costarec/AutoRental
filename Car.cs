af557003714ec831e91dfd2f307343a8a56933a3
costarec <costarec@uw.edu>
2023-01-16 02:40:09 -0800

    TINFO-200 Auto Rental Shop app

---
+++ Car.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace AutoRental
+{
+    // The class Car figures heavily in the operation of an auto rental
+    // business, so this is one of the fundamental object classes that
+    // our system will use.
+    internal class Car // : object
+    {
+        // currently, we are thinking that this class will consist
+        // mainly of properties to hold the data model and maybe
+        // 1 or 2 util functions
+
+        // Properties - all curently auto-implemented
+        public int YearModel { get; set; }
+        public string PaintColor { get; set; }
+        public string MakeModel { get; set; }
+        public decimal RentalPrice { get; set; }
+
+        // Constructors
+        // fully specified ctor providing all data needed for props
+        public Car(int year, string paint, string model, decimal price)
+        {
+            // assign all the passed in formal parameters to the properties
+            // above
+            YearModel = year;
+            PaintColor = paint;
+            MakeModel = model;
+            RentalPrice = price;
+        }
+
+        // adding back in default no-arg do nothing ctor
+        public Car()
+        {
+        }
+
+        // override the to string method so that we can print a car
+        // with all the data attributes having to do with a car
+        // overriding is a form of polymorphism
+        public override string ToString()
+        {
+            // declare a temp string var buffer
+            string str = string.Empty;
+
+            // fill the buffer with the data and info that we want to see printed out
+            str += "******************************************\n";
+            str += $" Year: {YearModel}\n";
+            str += $"Color: {PaintColor}\n";
+            str += $"Model: {MakeModel}\n";
+            str += $"Price: {RentalPrice}\n";
+
+
+            // return the string buffer that we created and filled
+            return str;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoRental
+{
+    // This app is intended to run a small auto rental business.
+    // This business specializes in muscle cars from the 70s and 80s
+    // Customers should be able to rent a car for day or multiple days.
+    // The Car as an object will figure heavily in the software functionality
+    // of the app. So we should have a Car class.
+    internal class Program
+    {
+        // main is simply the test driver for us to test the car class and other
+        // elements of the auto rental system.
+        static void Main(string[] args)
+        {
+            // I want to be able to make a car object when a customer rents one.
+            Car chuxCar = new Car();
+            Car bobzCar = new Car(1967, "Black", "Ford Mustang 2+2", 59.99m);
+            Car cindyzCar = new Car(1981, "Red", "Ferrari Boxer Berlinetta", 129.99m);
+            Car alanzCar = new Car(1969, "Blue", "Chevrolet Chevelle", 79.99m);
+
+            // I want to rent a 1979 silver porsche 930 turbo, rents for 119.99 per day
+            chuxCar.YearModel = 1979;
+            chuxCar.PaintColor = "Silver";
+            chuxCar.MakeModel = "Porsche 930 Turbo";
+            chuxCar.RentalPrice = 119.99m;
+
+            //// for now, manually print out the C# props that are in a "car" obj
+            //Console.WriteLine("******************************************");
+            //Console.WriteLine($" Year: {chuxCar.YearModel}");
+            //Console.WriteLine($"Color: {chuxCar.PaintColor}");
+            //Console.WriteLine($"Model: {chuxCar.MakeModel}");
+            //Console.WriteLine($"Price: {chuxCar.RentalPrice}");
+
+
+            // I want to be able to "print out" chuxCar as an object
+            Console.WriteLine(bobzCar);
+            Console.WriteLine(chuxCar);
+            Console.WriteLine(cindyzCar);
+            Console.WriteLine(alanzCar);
+
+            // block running program at the end - Enter key
+            Console.ReadLine();
+
+        }
+    }
+}
